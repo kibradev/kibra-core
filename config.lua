c033311e852fea1833324fb446c3007d58f9e94b
kibra <62299912+kibradev@users.noreply.github.com>
2022-12-21 21:33:36 +0300

    Add files via upload

--- config.lua
+++ config.lua
@@ -1,10 +1,15 @@
 Config = {}
 
-Config.Framework = "QBCore" -- or QBCore
+Config.Framework = "ESX" -- or QBCore
 
 Config.DrawMarkerRGBAColor = {2,47,255,0.8}
 
 Config.Events = {
     ["ESX"] = "esx:playerLoaded",
     ["QBCore"] = "QBCore:Client:OnPlayerLoaded"
+}
+
+Config.JobEvents = {
+    ["ESX"] = "esx:setJob",
+    ["QBCore"] = "QBCore:Client:OnJobUptade"
 }
