a55fa9d36f1f0971b5c15f81012693833f8348a3
kibra <62299912+kibradev@users.noreply.github.com>
2023-03-27 20:50:27 +0300

    Update main.lua

--- shared/main.lua
+++ shared/main.lua
@@ -4,16 +4,4 @@ Shared.Framework = "ESX" -- or "QBCore "
 
 Shared.Notify = "default" -- or "okok" (okokNotify)
 
-Shared.Events = {
-    ["ESX"] = {
-        "esx:playerLoaded",
-        "esx:setJob",
-        "esx:playerLogout"
-    },
-
-    ["QBCore"] = {
-        "QBCore:Client:OnPlayerLoaded",
-        "QBCore:Client:OnJobUpdate",
-        "QBCore:Client:OnPlayerUnload"
-    }
-}
+Shared.OldFramework = true
